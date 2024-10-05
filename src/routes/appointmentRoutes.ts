e4abf6cb4cae55d486490f53ffea56aa7016d469
anurag262000 <anuragmishra262000@gmail.com>
2024-10-05 12:31:45 +0530

    take 6

--- src/routes/appointmentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createAppointment } from '../controllers/appointmentController';
-
-const router: Router = Router();
-
-// Route to create a new appointment
-router.post('/appointment', createAppointment);
-
-export default router;
